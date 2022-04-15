9a7305d480899b0ea922b2f36d76d9fcddd02191
PlayerOne <playerprogramming@gmail.com>
2022-04-15 09:33:58 -0700

    Implement Poster, Title, Year in the content area

--- source/MainScreen.tsx
+++ source/MainScreen.tsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Animated, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  SafeAreaView,
+  Image,
+} from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
 import { TextInput } from "react-native-paper";
 
@@ -10,7 +17,7 @@ interface Props {
 
 export default function MainScreen(): React.ReactNode {
   const Titletranslation = useRef(new Animated.Value(0.8)).current;
-
+  const [ContentsTranslation, setContentsTranslation] = useState(true);
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState({ Title: "", Year: "", Poster: "" });
   const [search, setSearch] = useState("");
@@ -31,11 +38,12 @@ export default function MainScreen(): React.ReactNode {
   const onPress1 = () => {
     {
       Animated.timing(Titletranslation, {
-        toValue: 0.15,
+        toValue: 0.2,
         duration: 1000,
         useNativeDriver: false,
       }).start();
     }
+    setContentsTranslation(false);
   };
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: "pink" }]}>
@@ -53,39 +61,66 @@ export default function MainScreen(): React.ReactNode {
         </Text>
       </Animated.View>
 
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "grey",
-        }}
-      >
-        <AwesomeButton {...undefined} onPress={onPress1} width={300}>
-          <Text>Get Started</Text>
-        </AwesomeButton>
-      </View>
+      {ContentsTranslation && (
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "grey",
+          }}
+        >
+          <AwesomeButton {...undefined} onPress={onPress1} width={300}>
+            <Text>Get Started</Text>
+          </AwesomeButton>
+        </View>
+      )}
+      {!ContentsTranslation && (
+        <View
+          style={{
+            flex: 1,
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+            backgroundColor: "grey",
+          }}
+        >
+          <TextInput
+            style={{
+              margin: 20,
+              borderRadius: 100,
+              borderTopStartRadius: 100,
+              borderTopEndRadius: 100,
+            }}
+            placeholder="Search Movies"
+            onChangeText={(text) => setSearch(text)}
+            autoComplete={undefined}
+          />
 
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "grey",
-        }}
-      >
-        <AwesomeButton {...undefined} onPress={onPress1} width={300}>
-          <Text>Get Started</Text>
-        </AwesomeButton>
-      </View>
-
-      {/* <View style={{ flex: 1, backgroundColor: "blue" }}>
-        <TextInput
-          placeholder="Typing Something"
-          onChangeText={(text) => setSearch(text)}
-          autoComplete={undefined}
-        />
-      </View> */}
+          <View>
+            <View style={{ alignItems: "center", margin: 10 }}>
+              <Text style={{ fontSize: 30 }}>{data.Title}</Text>
+              <Text>{data.Year}</Text>
+            </View>
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                marginVertical: 10,
+              }}
+            >
+              <Image
+                style={{
+                  width: 250,
+                  height: 400,
+                  borderRadius: 30,
+                  marginBottom: 15,
+                }}
+                source={{ uri: data.Poster }}
+              />
+            </View>
+          </View>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
